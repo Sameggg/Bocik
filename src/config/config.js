7836e1f221544d44d31f2a7477091ced9045db44
Sameggg <68626622+Sameggg@users.noreply.github.com>
2020-10-29 08:24:55 +0100

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: "NzM1MDQ5NTA3NDIyNjAxMjI4.XxamMw.z-oATQsWzcWZCgaA9_sUpGJrTyA",
+  }
+  
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js")
+const chalk = require("chalk")
+
+const config = require("./config/config.js")
+
+const client = new Discord.Client()
+
+const log = console.log
+
+client.on("ready", () => {
+  log(chalk.green(`Zalogowano jako ${client.user.tag}!`))
+})
+
+client.on("message", (msg) => {
+  const { author } = msg
+
+  if (author.bot) {
+    return
+  }
+
+  if (msg.content === "!ping") {
+    msg.reply("Pong!")
+  }
+})
+
+client.login(config.token)
